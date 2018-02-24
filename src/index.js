497a57f980d423e47c7fddfbbb17e275ba33b4e7
Yury Staravoitau <MorfinAll@mail.ru>
2018-02-24 19:35:14 +0300

    Money-Exchange/feture: Implemented all logics and all tests passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" }
+    }
+    if (currency === 0) {
+        return {};
+    }
+
+    let result = {};
+
+    let cur = currency;
+    if (Math.floor(cur / 50) > 0) {
+        result.H = Math.floor(cur / 50);
+        cur = cur - Math.floor(cur / 50) * 50;
+    }
+    if (Math.floor(cur / 25) > 0) {
+        result.Q = Math.floor(cur / 25);
+        cur = cur - Math.floor(cur / 25) * 25;
+    }
+    if (Math.floor(cur / 10) > 0) {
+        result.D = Math.floor(cur / 10);
+        cur = cur - Math.floor(cur / 10) * 10;
+    }
+    if (Math.floor(cur / 5) > 0) {
+        result.N = Math.floor(cur / 5);
+        cur = cur - Math.floor(cur / 5) * 5;
+    }
+    if (cur > 0) {
+        result.P = cur;
+    }
+
+    return result;
 }
